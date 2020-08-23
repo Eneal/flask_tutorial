da1285301bc591f1ea368ac1dec3aeb9cbc9f031
Ezra Neal <ezra.g.d.neal@gmail.com>
2020-08-23 18:28:10 -0500

    template added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        'username': 'Ezra'
+    }
+    return render_template('index.html', title='Home', user=user)
